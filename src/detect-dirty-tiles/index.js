d77666b63e6d818dfb4e5866f28ac154138572f4
Lukas Martinelli <me@lukasmartinelli.ch>
2016-03-03 22:10:00 +0100

    Reintroduce some pg-promise improvements

--- src/detect-dirty-tiles/index.js
+++ src/detect-dirty-tiles/index.js
@@ -18,8 +18,8 @@ var db = pgp({
 var exportDir = process.env.EXPORT_DIR || '/data/export/';
 var dirtyTilesListFile = process.env.LIST_FILE || path.join(exportDir, 'tiles.txt');
 
-function queryChanges(timestamp, viewName) {
-    return db.query(
+function queryChanges(task, timestamp, viewName) {
+    return task.query(
         'SELECT osm_id, ST_AsGeoJSON(ST_Transform(geometry, 4326)) AS geometry ' +
         'FROM ' + viewName + ' ' +
         'WHERE timestamp=$1', [timestamp]
@@ -33,8 +33,8 @@ function queryChanges(timestamp, viewName) {
     });
 }
 
-function affectedTiles(timestamp, viewName, minZoomLevel, maxZoomLevel) {
-    return queryChanges(timestamp, viewName).then(function(rows) {
+function affectedTiles(task, timestamp, viewName, minZoomLevel, maxZoomLevel) {
+    return queryChanges(task, timestamp, viewName).then(function(rows) {
         return rows.map(function(row) {
             return {
                 view: viewName,
@@ -46,69 +46,71 @@ function affectedTiles(timestamp, viewName, minZoomLevel, maxZoomLevel) {
 }
 
 function recentDirtyViews() {
-    return db.one('SELECT MAX(timestamp) FROM osm_timestamps')
-    .then(function (result) {
-        var timestamp = result.max;
+    return db.task(function(t) {
+        return t.one('SELECT MAX(timestamp) FROM osm_timestamps')
+            .then(function (result) {
+                var timestamp = result.max;
 
-        function detectDirtyTiles(viewName, minZoomLevel, maxZoomLevel) {
-            return affectedTiles(timestamp, viewName, minZoomLevel, maxZoomLevel);
-        }
+                function detectDirtyTiles(viewName, minZoomLevel, maxZoomLevel) {
+                    return affectedTiles(t, timestamp, viewName, minZoomLevel, maxZoomLevel);
+                }
 
-        return q.all([
-            detectDirtyTiles('landuse_z5', 5, 5),
-            detectDirtyTiles('landuse_z6', 6, 6),
-            detectDirtyTiles('landuse_z7', 7, 7),
-            detectDirtyTiles('landuse_z8', 8, 8),
-            detectDirtyTiles('landuse_z9', 9, 9),
-            detectDirtyTiles('landuse_z10', 10, 10),
-            detectDirtyTiles('landuse_z11', 11, 11),
-            detectDirtyTiles('landuse_z12', 12, 12),
-            detectDirtyTiles('landuse_z13toz14', 13, 14),
-            detectDirtyTiles('waterway_z8toz12', 8, 12),
-            detectDirtyTiles('waterway_z13toz14', 13, 14),
-            detectDirtyTiles('water_z6toz12', 6, 12),
-            detectDirtyTiles('water_z13toz14', 13, 14),
-            detectDirtyTiles('aeroway_z12toz14', 12, 14),
-            detectDirtyTiles('barrier_line_z14', 14, 14),
-            detectDirtyTiles('landuse_overlay_z7', 7, 7),
-            detectDirtyTiles('landuse_overlay_z8', 8, 8),
-            detectDirtyTiles('landuse_overlay_z9', 9, 9),
-            detectDirtyTiles('landuse_overlay_z10', 10, 10),
-            detectDirtyTiles('landuse_overlay_z11toz12', 11, 12),
-            detectDirtyTiles('landuse_overlay_z13toz14', 13, 14),
-            detectDirtyTiles('tunnel_z12toz14', 12, 14),
-            detectDirtyTiles('road_z5to6', 5, 6),
-            detectDirtyTiles('road_z7', 7, 7),
-            detectDirtyTiles('road_z8to10', 8, 10),
-            detectDirtyTiles('road_z11', 11, 11),
-            detectDirtyTiles('road_z12', 12, 12),
-            detectDirtyTiles('road_z13', 13, 13),
-            detectDirtyTiles('road_z14', 14, 14),
-            detectDirtyTiles('bridge_z12to14', 12, 14),
-            detectDirtyTiles('admin_z2to6', 2, 6),
-            detectDirtyTiles('admin_z7to14', 7, 14),
-            detectDirtyTiles('place_label_z4toz5', 4, 5),
-            detectDirtyTiles('place_label_z6', 6, 6),
-            detectDirtyTiles('place_label_z7', 7, 7),
-            detectDirtyTiles('place_label_z8', 8, 8),
-            detectDirtyTiles('place_label_z9toz10', 9, 10),
-            detectDirtyTiles('place_label_z11toz12', 11, 12),
-            detectDirtyTiles('place_label_z13', 13, 13),
-            detectDirtyTiles('place_label_z14', 14, 14),
-            detectDirtyTiles('water_label_z10', 10, 10),
-            detectDirtyTiles('water_label_z11', 11, 11),
-            detectDirtyTiles('water_label_z12', 12, 12),
-            detectDirtyTiles('water_label_z13', 13, 13),
-            detectDirtyTiles('water_label_z14', 14, 14),
-            detectDirtyTiles('poi_label_z14', 14, 14),
-            detectDirtyTiles('road_label_z8toz10', 8, 10),
-            detectDirtyTiles('road_label_z11', 11, 11),
-            detectDirtyTiles('road_label_z12toz13', 12, 13),
-            detectDirtyTiles('road_label_z14', 14, 14),
-            detectDirtyTiles('waterway_label_z8toz12', 8, 12),
-            detectDirtyTiles('waterway_label_z13toz14', 13, 14)
-        ]);
-    }).then(_.flatten);
+                return q.all([
+                    detectDirtyTiles('landuse_z5', 5, 5),
+                    detectDirtyTiles('landuse_z6', 6, 6),
+                    detectDirtyTiles('landuse_z7', 7, 7),
+                    detectDirtyTiles('landuse_z8', 8, 8),
+                    detectDirtyTiles('landuse_z9', 9, 9),
+                    detectDirtyTiles('landuse_z10', 10, 10),
+                    detectDirtyTiles('landuse_z11', 11, 11),
+                    detectDirtyTiles('landuse_z12', 12, 12),
+                    detectDirtyTiles('landuse_z13toz14', 13, 14),
+                    detectDirtyTiles('waterway_z8toz12', 8, 12),
+                    detectDirtyTiles('waterway_z13toz14', 13, 14),
+                    detectDirtyTiles('water_z6toz12', 6, 12),
+                    detectDirtyTiles('water_z13toz14', 13, 14),
+                    detectDirtyTiles('aeroway_z12toz14', 12, 14),
+                    detectDirtyTiles('barrier_line_z14', 14, 14),
+                    detectDirtyTiles('landuse_overlay_z7', 7, 7),
+                    detectDirtyTiles('landuse_overlay_z8', 8, 8),
+                    detectDirtyTiles('landuse_overlay_z9', 9, 9),
+                    detectDirtyTiles('landuse_overlay_z10', 10, 10),
+                    detectDirtyTiles('landuse_overlay_z11toz12', 11, 12),
+                    detectDirtyTiles('landuse_overlay_z13toz14', 13, 14),
+                    detectDirtyTiles('tunnel_z12toz14', 12, 14),
+                    detectDirtyTiles('road_z5to6', 5, 6),
+                    detectDirtyTiles('road_z7', 7, 7),
+                    detectDirtyTiles('road_z8to10', 8, 10),
+                    detectDirtyTiles('road_z11', 11, 11),
+                    detectDirtyTiles('road_z12', 12, 12),
+                    detectDirtyTiles('road_z13', 13, 13),
+                    detectDirtyTiles('road_z14', 14, 14),
+                    detectDirtyTiles('bridge_z12to14', 12, 14),
+                    detectDirtyTiles('admin_z2to6', 2, 6),
+                    detectDirtyTiles('admin_z7to14', 7, 14),
+                    detectDirtyTiles('place_label_z4toz5', 4, 5),
+                    detectDirtyTiles('place_label_z6', 6, 6),
+                    detectDirtyTiles('place_label_z7', 7, 7),
+                    detectDirtyTiles('place_label_z8', 8, 8),
+                    detectDirtyTiles('place_label_z9toz10', 9, 10),
+                    detectDirtyTiles('place_label_z11toz12', 11, 12),
+                    detectDirtyTiles('place_label_z13', 13, 13),
+                    detectDirtyTiles('place_label_z14', 14, 14),
+                    detectDirtyTiles('water_label_z10', 10, 10),
+                    detectDirtyTiles('water_label_z11', 11, 11),
+                    detectDirtyTiles('water_label_z12', 12, 12),
+                    detectDirtyTiles('water_label_z13', 13, 13),
+                    detectDirtyTiles('water_label_z14', 14, 14),
+                    detectDirtyTiles('poi_label_z14', 14, 14),
+                    detectDirtyTiles('road_label_z8toz10', 8, 10),
+                    detectDirtyTiles('road_label_z11', 11, 11),
+                    detectDirtyTiles('road_label_z12toz13', 12, 13),
+                    detectDirtyTiles('road_label_z14', 14, 14),
+                    detectDirtyTiles('waterway_label_z8toz12', 8, 12),
+                    detectDirtyTiles('waterway_label_z13toz14', 13, 14)
+                ]);
+            });
+    });
 }
 
 recentDirtyViews().then(function (dirtyViews) {
